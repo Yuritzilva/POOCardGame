d0aa1ff8555ec4f7380c9e7ff5d7f4263d942824
Yuritzi <pyuitzi@gmail.com>
2025-11-25 10:07:01 -0800

    Updated mysql_env.py to adapt structure.sql queries execution

--- mysql/mysql_env.py
+++ mysql/mysql_env.py
@@ -1,30 +1,76 @@
-#This code will help to execute queries throught the program execution
+#This code will help to execute queries throught the program execution to create the base structure of the database
 
 # ---- IMPORTS ----
 import sys
 import os
+import mysql.connector
 #we need to append this line so db_connection can be pulled from the src file
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src')) 
 from db_connection import get_conn
 
-#First connection to the database, we will try to use the database
-def use_DB(conn):
-    query = """use CardGame"""
-    
+def create_database(conn):
     cursor = conn.cursor()
-    cursor.execute(query)
-    conn.commit()
-    print("Se usará CardGame como base de datos 0_0")
-    cursor.close()
+    try:
+        cursor.execute("CREATE DATABASE IF NOT EXISTS CardGame")
+        cursor.execute("USE CardGame")
+        print("Database is ready :)")
+    except mysql.connector.Error as err:
+        print(f"Error creating database: {err}")
+    finally:
+        cursor.close()
+
+
+def execute_sql_file(conn, filename= None):
+   
+    if filename is None:
+        filename = os.path.join("mysql", "structure.sql")
+
+    if not os.path.exists(filename):
+        print(f"Error: File '{filename}' wasn't found")
+        return
+        
+    cursor = conn.cursor()
+    
+    try:
+        with open(filename, 'r') as f:
+            
+            sql_script = f.read()
+            
+            # Divide scripts using ; and ignore comments
+            commands = [cmd.strip() for cmd in sql_script.split(';') if cmd.strip() and not cmd.strip().startswith('--')]
+
+            success_count = 0
+            for command in commands:
+                try:
+                    cursor.execute(command)
+                    success_count += 1
+                except mysql.connector.Error as err:
+                    print(f"Failed querie: {command[:50]}...")
+                    print(f"   Error: {err}")
+                    
+            conn.commit()
+            print(f"Execution finished, queries executed: {success_count}")
+
+    except Exception as e:
+        print(f"Lecture error in SQL file: {e}")
+        conn.rollback() # Revert
+    finally:
+        cursor.close()
 
 def main():
     conn = get_conn()
 
     if conn:
-        use_DB(conn)
+        create_database(conn)
+        file_path = os.path.join("mysql", "structure.sql")
+        execute_sql_file(conn, file_path)
+        
+        print("\n----SHOW TABLES FOR CARD GAME-----")
+        cursor = conn.cursor()
+        cursor.execute("SHOW TABLES")
+        print(cursor.fetchall())
         conn.close()
-    else:
-        print("no se puede usar la BD")
+
         
 if __name__ == "__main__":
     main()
