f811ce9557e28cf0026d8ed628578f90031ed083
Yuritzi <pyuitzi@gmail.com>
2025-11-26 18:34:50 -0800

    Built card and deck classes, could still suffer some modifications

--- src/card.py
+++ src/card.py
@@ -1,1 +1,14 @@
 #Card class
+
+from db_connection import get_conn
+class Card:
+    def __init__(self, id_, value, suit):
+        self.id = id_
+        self.value = value
+        self.suit = suit
+        
+    def __str__(self):
+        return f"{self.value}{self.suit}"
+
+    def __repr__(self):
+        return self.__str__()
