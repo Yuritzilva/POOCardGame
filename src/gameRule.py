c290e0312d2cee3c5bce105c4c03892739061890
Yuritzi <pyuitzi@gmail.com>
2025-11-26 21:59:40 -0800

    Created class hand

--- src/gameRule.py
+++ src/gameRule.py
@@ -1,1 +1,16 @@
 #gameRule class
+
+class GameRule:
+    def __init__(self, id_, name, value):
+        self.id = id_
+        self.name = name
+        self.value = value
+        self.combination = []
+
+
+    @classmethod
+    def set_GameRule():
+        pass
+
+    def get_GameRule():
+        pass
